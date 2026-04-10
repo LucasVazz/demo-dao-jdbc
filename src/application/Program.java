077a25aaf37a25ca97e09babe6a4e2d75c3f2a6b
Lucas Vaz <lucasvaz164@gmail.com>
2026-04-10 15:24:17 -0300

    at3

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,21 +3,25 @@ package application;
 import java.text.DateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-	
-	
+
 		Department obj = new Department(1, "teste");
 		System.out.println(obj);
 
 		Seller sl = new Seller(1, "lucas", "lucasvaz@gmail.com", new Date(), 3434.34, obj);
-		
+
 		System.out.println(sl);
-		
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println(sl);
+
 	}
 
 }
